808495a22fdaf5973f9ade4de24b6ed48953e2fa
WP <helenapaloheimo@gmail.com>
2022-10-13 16:10:33 +0300

    Initial Commit

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+export interface ApiResult {
+  page: number;
+  results: any[];
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  total_pages: number;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  total_results: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+  constructor(private http: HttpClient) {}
+
+  getTopRatedMovies(page = 1): Observable<ApiResult> {
+    return this.http.get<ApiResult>(
+      `${environment.baseUrl}/movie/popular?api_key=${environment.apiKey}&page=${page}`
+    );
+  }
+
+  getMovieDetails(id: string) {
+    return this.http.get(
+      `${environment.baseUrl}/movie/${id}?api_key=${environment.apiKey}`
+    );
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiKey: '65d5dbfe60dce7db8676791fffdfd3d0',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'https://image.tmdb.org/t/p'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
